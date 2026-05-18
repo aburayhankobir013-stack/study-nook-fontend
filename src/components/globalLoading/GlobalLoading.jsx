8f810378e2301bc5e42c5a2f18eddf88664005a7
ABU-RAYHAN-KOBIR <aburayhankobir013@gmail.com>
2026-05-18 13:20:27 +0600

    Global loading page included nicely based on screen sizes condition

--- src/components/globalLoading/GlobalLoading.jsx
+++ src/components/globalLoading/GlobalLoading.jsx
@@ -1,10 +1,20 @@
+import { Spinner } from "@heroui/react";
 
-
-
-export default function GlobalLoading () {
+export default function GlobalLoading() {
   return (
-    <h1>
-      I am global loading component!
-    </h1>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="xs:hidden">
+        <Spinner size="sm" />
+      </div>
+      <div className="hidden xs:block">
+        <Spinner size="md" />
+      </div>
+      <div className="hidden md:block">
+        <Spinner size="lg" />
+      </div>
+      <div className="hidden lg:block">
+        <Spinner size="xl" />
+      </div>
+    </div>
   );
 }
