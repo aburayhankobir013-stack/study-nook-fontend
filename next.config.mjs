08f6c27cf1c9cc00f7112e2f19d2b0d2f45c4af7
ABU-RAYHAN-KOBIR <aburayhankobir013@gmail.com>
2026-05-18 12:20:44 +0600

    Configuring next.config.mjs file for accessing all external images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  allowedDevOrigins: ['192.168.1.101'],
 };
 
 export default nextConfig;
