05a225644d8d8b045fa49f6b3953bd8e15196864
ABU-RAYHAN-KOBIR <aburayhankobir013@gmail.com>
2026-05-22 10:49:48 +0600

    Available room title, sub-title and button included

--- src/components/card/HomeCardContainer.jsx
+++ src/components/card/HomeCardContainer.jsx
@@ -1,12 +1,37 @@
+import { Button } from "@heroui/react";
 import Card from "./Card";
+import Link from "next/link";
+import { GoPlus } from "react-icons/go";
 
-
-export default async function HomeCardContainer () {
+export default async function HomeCardContainer() {
   const response = await fetch(`http://localhost:5000`);
   const limitedRooms = await response.json();
   return (
-    <div className="container mx-auto border grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4 bg-green-100">
-      {limitedRooms.map((room) => <Card key={room._id} room={room}/>)}
+    <div className="container mx-auto border p-4 bg-green-100">
+      <div className="flex flex-col md:flex-row md:justify-between justify-center items-center md:text-left text-center gap-2 p-4">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-lg xs:text-xl text-green-900 font-bold">
+            Available Study Rooms
+          </h1>
+          <p className="font-semibold text-green-700">
+            Hand-picked rooms recently added to Study Nook
+          </p>
+        </div>
+        <Link href={"/add_room"}>
+          <Button
+            variant="outline"
+            className="rounded-xs bg-green-500 text-white font-bold"
+          >
+            <GoPlus />
+            <span>Add Room</span>
+          </Button>
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        {limitedRooms.map((room) => (
+          <Card key={room._id} room={room} />
+        ))}
+      </div>
     </div>
   );
 }
